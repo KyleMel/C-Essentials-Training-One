f07b62bc6486e0494b500a31d94c010c81c537b8
Kyle Melton <KyleMeltonCodes@gmail.com>
2021-08-29 23:41:37 -0500

    Encapsulation with access modifiers

--- School.cs
+++ School.cs
@@ -8,14 +8,14 @@ namespace Csharp_Essential_Training_One
 {
     class School
     {
-        string Name { get; set; }
-        string Address { get; set; }
-        string City { get; set; }
-        string State { get; set; }
-        string Zip { get; set; }
-        string Number { get; set; }
-        string _twitterAddress;
-        string TwitterAddress
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Number { get; set; }
+        private string _twitterAddress;
+        public string TwitterAddress
         {
             //make sure the twitter address starts with an @
             get { return _twitterAddress; }
