596faf9ec0ec1bfc5f9f9cebce55b3e6a8a2bdf5
Kyle Melton <KyleMeltonCodes@gmail.com>
2021-08-29 22:13:57 -0500

    start of project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_Essential_Training_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
