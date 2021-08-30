c3777b759b1f8714a8f68568807353b5025e338b
Kyle Melton <KyleMeltonCodes@gmail.com>
2021-08-29 23:28:01 -0500

    creating a class

---
+++ School.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Essential_Training_One
+{
+    class School
+    {
+        string SchoolName;
+        string SchoolAddress;
+        string SchoolCity;
+        string SchoolState;
+        string SchoolZip;
+        string PhoneNumber;
+
+    }
+}
