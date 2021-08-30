afb17c4f804d7aa6ecbcf98545767ae6ebf61792
Kyle Melton <KyleMeltonCodes@gmail.com>
2021-08-29 23:50:31 -0500

    creating methods

--- School.cs
+++ School.cs
@@ -13,7 +13,7 @@ namespace Csharp_Essential_Training_One
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
-        public string Number { get; set; }
+        public string PhoneNumber { get; set; }
         private string _twitterAddress;
         public string TwitterAddress
         {
@@ -30,6 +30,27 @@ namespace Csharp_Essential_Training_One
                     throw new Exception("The twitter address must begin with @");
                 }
             }
+        }
+        public School()
+        {
+            Name = "Untitled School";
+            PhoneNumber = "555-1234";
+        }
+        public School(string SchoolName, string SchoolPhoneNumber)
+        {
+            Name = SchoolName;
+            PhoneNumber = SchoolPhoneNumber;
+        }
+
+        public float AverageThreeScores(float a, float b, float c)
+        {
+            var result = (a + b + c) / 3;
+            return result;
+        }
+        public int AverageThreeScores(int a, int b, int c)
+        {
+            var result = (a + b + c) / 3;
+            return result;
         }
     }
 }
