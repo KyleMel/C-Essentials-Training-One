8e3aecf1aebcbf2b75fd83b0537177d04c04d971
Kyle Melton <KyleMeltonCodes@gmail.com>
2021-08-29 22:53:17 -0500

    string formatters.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Csharp_Essential_Training_One
 {
@@ -6,7 +7,10 @@ namespace Csharp_Essential_Training_One
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var city = "Dallas";
+            var temperature = 103.4f;
+            var currentDt = DateTime.Now;
+            Console.WriteLine(string.Format("Welcome to {0}. The time is {1:t}. The temperature is {2:0.00}.", city, currentDt, temperature));
         }
     }
 }
