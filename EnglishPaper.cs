36f50d1071d2e6df469f24841ee6df11ebb5511a
Kyle Melton <KyleMeltonCodes@gmail.com>
2021-08-30 19:11:47 -0500

    adding interfaces

---
+++ EnglishPaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Essential_Training_One
+{
+    public class EnglishPaper
+    {
+        public string Title { get; set; }
+        public int MinimumWordCount { get; set; }
+        public string PaperText { get; set; }
+    }
+}
